117022488a3268c88a8271de3f017052d7163d17
Stefan Olaru <contact@stefanolaru.com>
2021-08-01 10:17:31 +0300

    projects CRUD

---
+++ src/lib/entities/project.js
@@ -0,0 +1,162 @@
+const AWS = require("aws-sdk"),
+    { v4: uuid } = require("uuid");
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+});
+
+const ddb = new AWS.DynamoDB(),
+    eb = new AWS.EventBridge();
+
+/**
+ * 	Project Get
+ * 	retrieve the project data from DB
+ */
+
+module.exports.get = async (id) => {
+    return new Promise((resolve, reject) => {
+        ddb.getItem({
+            TableName: process.env.DDB_TABLE,
+            Key: AWS.DynamoDB.Converter.marshall({
+                entity: "project",
+                id: id,
+            }),
+        })
+            .promise()
+            .then((res) => {
+                if (res.Item) {
+                    let item = AWS.DynamoDB.Converter.unmarshall(res.Item);
+                    resolve(item);
+                }
+
+                // reject if no project was found
+                reject("Project not accessible.");
+            })
+            .catch((err) => reject(err.message));
+    });
+};
+
+/**
+ *  Project Create - requires  data.name
+ * 	returns project object
+ */
+module.exports.create = async (data) => {
+    return new Promise((resolve, reject) => {
+        // assign default values
+        Object.assign(data, {
+            entity: "project",
+            id: uuid(),
+            // name: 'Project name',
+            created_at: Math.floor(+new Date() / 1000),
+            // variables: {},
+            // notifications: {},
+            tests_count: 0,
+        });
+
+        console.log(data);
+
+        // write to DB
+        ddb.putItem({
+            TableName: process.env.DDB_TABLE,
+            Item: AWS.DynamoDB.Converter.marshall(data),
+        })
+            .promise()
+            .then(() => {
+                // create the eventbus
+                return eb
+                    .createEventBus({
+                        Name: "uptimemonitor_" + data.id,
+                    })
+                    .promise();
+            })
+            .then(() => resolve(data))
+            .catch((err) => {
+                console.log(err);
+                reject(err.message);
+            });
+    });
+};
+
+/**
+ * 	Project Update
+ * 	retuns true or error
+ */
+module.exports.update = async (id, data) => {
+    return new Promise((resolve, reject) => {
+        // init empty stuff here
+        let ean = {}; // expression attribute names
+        let eav = {}; // expression attribute values
+        let uexpr = []; // update expr arr
+
+        // remove id from data if present
+        let pdata = Object.assign({}, data);
+
+        // fields that can be updated
+        const key_whitelist = [
+            "invocations_count",
+            "name",
+            "tests_count",
+            "variables",
+            "notifications",
+        ];
+
+        //
+        // loop all data keys, remove the keys that are not present in whitelist
+        Object.keys(pdata).forEach((key) => {
+            if (key_whitelist.indexOf(key) == -1) {
+                delete pdata[key];
+            }
+        });
+
+        // add updated flag
+        pdata["updated_at"] = Math.round(+new Date() / 1000);
+
+        // loop obj keys
+        Object.keys(pdata).forEach((key) => {
+            ean["#" + key] = key.toString();
+            eav[":" + key] = pdata[key];
+            uexpr.push("#" + key + "=:" + key);
+        });
+
+        // update db
+        ddb.updateItem({
+            TableName: process.env.DDB_TABLE,
+            Key: AWS.DynamoDB.Converter.marshall({
+                entity: "project",
+                id: id,
+            }),
+            ExpressionAttributeNames: ean,
+            ExpressionAttributeValues: AWS.DynamoDB.Converter.marshall(eav),
+            UpdateExpression: "SET " + uexpr.join(", "),
+        })
+            .promise()
+            .then(() => resolve(true))
+            .catch((err) => reject(err.message));
+    });
+};
+
+/**
+ * Project delete
+ * requires project ID
+ * returns true or error
+ */
+
+module.exports.delete = async (id) => {
+    return new Promise((resolve, reject) => {
+        ddb.deleteItem({
+            TableName: process.env.DDB_TABLE,
+            Key: AWS.DynamoDB.Converter.marshall({ entity: "project", id: id }),
+        })
+            .promise()
+            .then(() => {
+                // cleanup the mess
+                // 1) delete all tests within project
+                // 2) delete all logs within project ? maybe not, they have TTL
+                // 3) delete all eventbus rules
+                // 4) delete the eventbus
+            })
+            .then(() => resolve(true))
+            .catch((err) => reject(err.message));
+    });
+};
---
+++ src/projects.js
@@ -0,0 +1,80 @@
+const Project = require("./lib/entities/project");
+
+// projects handler
+exports.handler = async (event) => {
+    //
+    // prepare the response object
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+            "Access-Control-Allow-Methods": "GET,POST,PATCH,DELETE,OPTIONS",
+        },
+        body: "",
+    };
+
+    switch (event.httpMethod) {
+        // get tasks or task
+        case "GET":
+            if (event.pathParameters.id) {
+                // get project
+                response.body = await Project.get(id)
+                    .then()
+                    .catch((err) => {
+                        return {
+                            message: err,
+                        };
+                    });
+            } else {
+                // list the projects here
+                // later !!
+            }
+            break;
+
+        case "POST":
+            console.log(JSON.parse(event.body));
+            await Project.create(JSON.parse(event.body))
+                .then((res) => {
+                    response.body = res;
+                })
+                .catch((err) => {
+                    response.statusCode = 400;
+                    response.body = {
+                        message: err,
+                    };
+                });
+            break;
+
+        case "PATCH":
+            await Project.update(
+                event.pathParameters.id,
+                JSON.parse(event.body)
+            )
+                .then((res) => {
+                    response.body = res;
+                })
+                .catch((err) => {
+                    response.statusCode = 400;
+                    response.body = {
+                        message: err,
+                    };
+                });
+            break;
+
+        case "DELETE":
+            await Project.delete(event.pathParameters.id)
+                .then((res) => {
+                    response.body = res;
+                })
+                .catch((err) => {
+                    response.statusCode = 400;
+                    response.body = { message: err };
+                });
+            break;
+    }
+
+    // output the results
+    response.body = JSON.stringify(response.body);
+    return response;
+};
