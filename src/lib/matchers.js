997d74276d9f184a0c18abf6fe0b831f645b1bd8
Stefan Olaru <contact@stefanolaru.com>
2021-08-06 07:20:18 +0300

    remove obsolete matchers

--- src/lib/matchers.js
+++ src/lib/matchers.js
@@ -1,6 +1,9 @@
 const expect = require("expect");
 
+// expect(selector).toMatch(context, val)
+
 async function toBePresent(selector, page, is_visible = false) {
+    // console.log(selector);
     const pass = await page.evaluate(
         (selector, is_visible) => {
             const el = document.querySelector(selector);
@@ -40,105 +43,7 @@ async function toBeVisible(selector, page) {
     };
 }
 
-async function elementAttribute(
-    selector,
-    page,
-    attribute,
-    matcher,
-    value = null
-) {
-    const attr = await page.evaluate(
-        (selector, attribute) => {
-            const el = document.querySelector(selector);
-            // otherwise return the get attribute
-            return el ? el.getAttribute(attribute) : !!el;
-        },
-        selector,
-        attribute
-    );
-
-    let pass = true;
-
-    //
-    try {
-        matcher.startsWith("not.")
-            ? expect(attr).not[matcher.replace("not.", "")](value)
-            : expect(attr)[matcher](value);
-    } catch (e) {
-        pass = false;
-    }
-
-    return {
-        message: () =>
-            pass
-                ? "Attribute matches condition."
-                : "Attribute does not match condition.",
-        pass: pass,
-    };
-}
-
-async function styleProperty(selector, page, property, matcher, value = null) {
-    const propval = await page.evaluate(
-        (selector, property) => {
-            const el = document.querySelector(selector);
-            return window.getComputedStyle(el).getPropertyValue(property);
-        },
-        selector,
-        property
-    );
-
-    console.log(propval);
-
-    let pass = true;
-
-    //
-    try {
-        matcher.startsWith("not.")
-            ? expect(propval).not[matcher.replace("not.", "")](value)
-            : expect(propval)[matcher](value);
-    } catch (e) {
-        pass = false;
-    }
-
-    return {
-        message: () =>
-            pass
-                ? "Style property matches condition."
-                : "Style property does not match condition.",
-        pass: pass,
-    };
-}
-
-async function textContent(selector, page, matcher, value = null) {
-    const text = await page.evaluate((selector) => {
-        const el = document.querySelector(selector);
-        return el ? el.innerText.trim() : !!el;
-    }, selector);
-
-    let pass = true;
-
-    //
-    try {
-        matcher.startsWith("not.")
-            ? expect(text).not[matcher.replace("not.", "")](value)
-            : expect(text)[matcher](value);
-    } catch (e) {
-        pass = false;
-    }
-
-    return {
-        message: () =>
-            pass
-                ? "Text content matches condition."
-                : "Text does not match condition.",
-        pass: pass,
-    };
-}
-
 module.exports = {
     toBePresent,
     toBeVisible,
-    elementAttribute,
-    styleProperty,
-    textContent,
 };
